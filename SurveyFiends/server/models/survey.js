17e7ccf0a4146993ce544d449544a30cef9ebe34
sayus61894 <parth.shreyash.patel@gmail.com>
2020-11-12 16:56:01 -0500

    Created openSurveys Page, create get page for selected survey; working on post for selected survey

--- SurveyFiends/server/models/survey.js
+++ SurveyFiends/server/models/survey.js
@@ -1,9 +1,15 @@
 let mongoose = require('mongoose');
 
+let thirtyDaysFromNow = ()=>{
+    let date = new Date(Date.now()+ 30*24*60*60*1000);
+    return date;
+};
+
 let surveyModel = mongoose.Schema({
     authorName: String,
     surveyName: String,
-    authorPubDate: {type: Date, default: Date.now}
+    authorPubDate: {type: Date, default: Date.now},
+    expiryDate: {type:Date, default: thirtyDaysFromNow}
 },
 {
     collection: "Surveys"
--- SurveyFiends/server/routes/index.js
+++ SurveyFiends/server/routes/index.js
@@ -1,5 +1,11 @@
-var express = require('express');
-var router = express.Router();
+let express = require('express');
+let router = express.Router();
+let mongoose = require('mongoose');
+
+// connect to our Survey Model
+let Survey = require('../models/survey');
+let Question = require('../models/question');
+let Answer = require('../models/survey');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -16,19 +22,40 @@ router.get('/about', function(req, res, next) {
   res.render('index', { title: 'About'});
 });
 
-/* GET Products page. */
-router.get('/products', function(req, res, next) {
-  res.render('index', { title: 'Products'});
+/* GET openSurveysList page. */
+router.get('/openSurveysList', function(req, res, next) {
+  let now = Date.now();
+  Survey.find({expiryDate: {$gte: now}}).exec((err, openSurveys)=>{
+    if(err){
+      return console.log(err);
+    }else{
+      res.render('openSurveys/openSurveysList', { title: 'Open Surveys', Surveys: openSurveys});
+    };
+  });
 });
 
-/* GET Services page. */
-router.get('/services', function(req, res, next) {
-  res.render('index', { title: 'Services'});
-});
+// GET selected survey page
+router.get('/openSurveysList/:id', (req, res, err)=>{
+  let surveyid = req.params.id;
+  Survey.findById(surveyid, (err, survey) => {
+    if(err){
+      return console.log(err);
+    }else{
+      Question.find({surveyID: surveyid}).sort('questionsNumber').exec((err, SurveyQuestions)=>{
+        if(err){
+          return console.log(err);
+        }else{
+          res.location(SurveyQuestions[0]._id);
+          res.render('openSurveys/displaySurvey',{title:'Open Surveys', Survey: survey, Questions: SurveyQuestions});
+        }
+      })
+    }
+  })
+})
 
-/* GET Contact Us page. */
-router.get('/contact', function(req, res, next) {
-  res.render('index', { title: 'Contact'});
+
+router.post('/openSurveysList/:id', (req, res, err)=>{
+  
 });
 
 module.exports = router;
