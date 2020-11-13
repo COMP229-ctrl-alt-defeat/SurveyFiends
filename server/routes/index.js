ff0a75aadd8b7b4024b9c4e5fbcaab45150e0e23
Parth Shreyash Patel <parth.shreyash.patel@gmail.com>
2020-11-13 16:04:43 -0500

    Home and About page sent by Arshdeep

--- server/routes/index.js
+++ server/routes/index.js
@@ -28,7 +28,7 @@ router.get('/home', function(req, res, next) {
 
 /* GET About Us page. */
 router.get('/about', function(req, res, next) {
-  res.render('index', { title: 'About'});
+  res.render('about', { title: 'About'});
 });
 
 /* GET openSurveysList page. */
