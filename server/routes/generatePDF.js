fe8adffe25225e9432f9bf1ebe3c1fbb78fee238
Parth Shreyash Patel <parth.shreyash.patel@gmail.com>
2020-12-08 08:13:17 -0500

    Minor fix; testing pdf generation

--- server/routes/generatePDF.js
+++ server/routes/generatePDF.js
@@ -1,7 +1,14 @@
 let express = require('express');
 let router = express.Router();
+let pdfDoc = require('pdfkit');
 
+router.get('/:id', (req, res, next) => {
+    let pdf = new pdfDoc();
+    pdf.text("trial text");
+    pdf.pipe(res);
 
+    pdf.end();
+})
 
 
 module.exports = router;
